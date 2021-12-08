8246e76c9da7b8b372f5efcc9d343e4e9b2a849b
ajlic24@gmail.com <ajlic24@gmail.com>
2021-12-08 16:52:39 -0500

    commit tree 5fcfb960c9fc72f5b17c2bfcb6bd243355ee45bc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  document.querySelector(`#button`).addEventListener(`click`, (e) => {
+      e.preventDefault()
+      const input = document.querySelector(`#searchByID`).value
+
+      fetch(`http://localhost:3000/movies/${input}`)
+      .then(resp => resp.json())
+      .then(data => {
+        const title = document.querySelector(`#movieDetails h4`)
+        const summary = document.querySelector(`#movieDetails p`)
+        title.textContent = data.title
+        summary.textContent = data.summary
+      })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+init()
